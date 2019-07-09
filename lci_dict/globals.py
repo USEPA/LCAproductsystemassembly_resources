2fe52543a4492f7d5713b7d5d03b7d86d53fca3c
WesIngwersen <ingwersen.wesley@epa.gov>
2019-07-09 14:47:18 -0400

    Add in default modulepath, change NAICS to generic

--- lci_dict/globals.py
+++ lci_dict/globals.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import os
 
-bdata_folder = 'data-background/'
+try:
+    modulepath = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/'
+except NameError:
+    modulepath = 'lci_dict/'
+
+outputpath = modulepath + 'output/'
+
+bdata_folder = modulepath + '../data-background/'
 default_location = 'US'
-naics_category = '31-33: Manufacturing/3364: Aerospace Product and Parts Manufacturing/'
+naics_category = 'NAICS-2/NAICS-4'
 default_product_flow_category = ''
 
 default_unit_name = 'Item(s)'
